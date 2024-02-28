c423303161d3c6118c91c746d9dad7f4b3f6dd8b
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2024-02-29 01:36:32 +0530

    Update 003.with_images_input.py

--- OpenCV/003.with_images_input.py
+++ OpenCV/003.with_images_input.py
@@ -5,7 +5,7 @@ ankit=cv2.imread('lena.jpg',1)
 print(ankit)
 
 cv2.imshow('image',ankit)#image is the window name that will open the image
-k = cv2.waitKey(0) & 0xFF #In the documentation, it is written to use this.
+k = cv2.waitKey(0) & 0xFF #In the documentation, it is written to use this mask 0xFF.
 
 # When ESC key is pressed then close the window
 if k==27: #ESC key has the value 27
